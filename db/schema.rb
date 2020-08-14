744ec2355312f4ae150dda1a01b1616bc7b14f78
Ben Zelinski <bziggz@gmail.com>
2020-08-13 21:02:35 -0400

    Comments Functionality

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_13_200359) do
+ActiveRecord::Schema.define(version: 2020_08_13_202617) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "creator"
+    t.text "body"
+    t.integer "ticket_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "projects", force: :cascade do |t|
     t.string "name"
